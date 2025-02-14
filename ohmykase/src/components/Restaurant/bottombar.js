b4a538489b2774fdc498f2803bda2133d3b2e85a
hofkj <carrot070410@naver.com>
2025-02-14 23:25:59 +0900

    Feat : 레스토랑 페이지 하단바 추가

---
+++ ohmykase/src/components/Restaurant/bottombar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from '../../styles/Restaurant/bottombar.module.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function Bottombar() {
+    return (
+        <div className={styles.bottombar_container}>
+            <div><img src="/images/icon/bookmark.png" alt="bookmark" className={styles.bookmark} /></div>
+            {/* <i className={`bi bi-bookmark-fill ${styles.bookmark}`}></i> */}
+            <button className={styles.reservation_button}>예약하기</button>
+        </div>
+    );
+}
+
+export default Bottombar;
---
+++ ohmykase/src/pages/RestaurantPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Bottombar from "../components/Restaurant/bottombar";
+
+function RestaurantPage() {
+    return (
+        <div>
+            <Bottombar/>
+        </div>
+    );
+}
+
+export default RestaurantPage;
