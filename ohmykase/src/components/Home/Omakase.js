f788197a107f55e906156ec404afd19e23c075be
hofkj <carrot070410@naver.com>
2025-02-18 14:44:10 +0900

    Feat : omakase 추가

---
+++ ohmykase/src/components/Home/Omakase.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from '../../styles/home/omakase.module.css';
+
+function Popularity() {
+    return (
+        <div className={styles.omakaseContainer}>
+            <div className={styles.omakaseHeader}>
+                <div className={styles.omakaseTitle}>인기 오마카세</div>
+                <div className={styles.omakaseSubtitle}>
+                    이용객들이 만족한 오마카세를 확인해보세요
+                </div>
+            </div>
+
+            <div className={styles.omakaseMore}>
+                <div>더보기 <img src="/images/icon/arrow.png" className={styles.arrow}/></div>
+            </div>
+        </div>
+    );
+}
+
+export default Popularity;
