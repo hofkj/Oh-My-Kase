13c39fab035a6a63f6ade24c4e0a95eedb1ebb1d
hofkj <carrot070410@naver.com>
2025-02-18 15:07:01 +0900

    Design : 식당 카드 css 수정

--- ohmykase/src/components/Home/restaurant.js
+++ ohmykase/src/components/Home/restaurant.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../../styles/Home/restaurant.module.css';
+import styles from '../../styles/home/restaurant.module.css';
 
 function Restaurant() {
     return (
