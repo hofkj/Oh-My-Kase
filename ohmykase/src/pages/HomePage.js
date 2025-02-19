706e41e6a84bfb4a5295a5c304331f0c053287e8
hofkj <carrot070410@naver.com>
2025-02-19 11:34:47 +0900

    Feat : 홈 페이지

--- ohmykase/src/pages/HomePage.js
+++ ohmykase/src/pages/HomePage.js
@@ -1,11 +1,32 @@
 import React from "react";
-import Nav from '../components/Home/nav';
-import Restaurant from "../components/Home/restaurant";
+import Header from "../components/home/Header";
+import Benner from "../components/home/Benner";
+import LocationDropdown from "../components/home/LocationDropdown";
+import MenuDropdown from "../components/home/MenuDropdown";
+import Omakase from "../components/home/Omakase";
+import RestaurantSwiper from "../components/home/RestaurantSwiper";
+import Nav from '../components/home/Nav';
+
+import styles from '../styles/pages/homePage.module.css';
 
 function HomePage() {
     return (
-        <div>
-            <Restaurant />
+        <div className={styles.pageContainer} >
+            <Header />
+            <Benner />
+
+            <div className={styles.dropdownContainer}>
+                <LocationDropdown />
+                <MenuDropdown />
+            </div>
+
+            <Omakase />
+            <RestaurantSwiper />
+
+            <Omakase />
+            <RestaurantSwiper />
+        
+
             <Nav />
         </div>
     );
