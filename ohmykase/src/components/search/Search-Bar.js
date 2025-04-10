207207396132a99ffa0264f2ca1ae0973032d8e3
hofkj <carrot070410@naver.com>
2025-04-10 19:32:04 +0900

    Feat : 서치바 백버튼 추가

--- ohmykase/src/components/search/Search-Bar.js
+++ ohmykase/src/components/search/Search-Bar.js
@@ -1,9 +1,25 @@
 import React from "react";
 import styles from "../../styles/search/searchBar.module.css";
 
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+
 function SearchBar() {
+
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  };
+ 
+
+
   return (
     <div className={styles.container}>
+      <img src="/images/icon/header_arrow.png" className={styles.backBtn} onClick={handleClick} />
+      
       <div className={styles.inputContainer}>
         <img src="../../images/icon/search_icon.png" className={styles.searchIcon}></img>
         <input placeholder="검색" type="text" className={styles.input}></input>
