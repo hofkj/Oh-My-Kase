bace6d3c5d9c586774f6d3a440478f2f165be896
hofkj <carrot070410@naver.com>
2025-04-12 13:47:08 +0900

    Feat : 예약하기 버튼 클릭 -> 캘린더 팝업

--- ohmykase/src/components/Restaurant/bottombar.js
+++ ohmykase/src/components/Restaurant/bottombar.js
@@ -1,22 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/restaurant/bottombar.module.css";
-// import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { useEffect, useState } from "react";
-
-function Bottombar() {
+function Bottombar({ onClickReserve }) {
   const [isBookmarked, setIsBookmarked] = useState(false);
+
   const toggleBookmark = () => {
     setIsBookmarked((prevState) => !prevState);
-  };
-
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/ReservationPage");
-    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
@@ -32,11 +21,11 @@ function Bottombar() {
           className={styles.bookmark}
           onClick={toggleBookmark}
           style={{ cursor: "pointer" }}
-        />{" "}
+        />
         <div className={styles.bookmarkNum}>2,911</div>
       </div>
-      {/* <i className={`bi bi-bookmark-fill ${styles.bookmark}`}></i> */}
-      <button className={styles.reservation_button} onClick={handleClick}>
+
+      <button className={styles.reservation_button} onClick={onClickReserve}>
         예약하기
       </button>
     </div>
