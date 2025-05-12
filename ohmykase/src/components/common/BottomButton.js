7dc47870ff382a76a44379297882c3a31d0cecb6
hofkj <carrot070410@naver.com>
2025-05-13 01:56:42 +0900

    Feat : 예약 페이지1 컴포넌트 커밋

--- ohmykase/src/components/common/BottomButton.js
+++ ohmykase/src/components/common/BottomButton.js
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from "../../styles/common/bottomButton.module.css";
+import { useNavigate } from "react-router-dom";
 
-function BottomButton(props) {
-    return (
-        <div className={styles.buttonContainer}>
-            <button className={styles.btn}>{props.text}</button>
-        </div>
-    )
+function BottomButton({ text, navigateTo }) {
+  const navigate = useNavigate();
+
+  // 버튼 클릭 시 지정된 경로로 이동
+  const handleClick = () => {
+    navigate(navigateTo); // navigateTo로 이동
+    window.scrollTo({ top: 0, behavior: "smooth" }); // 페이지 맨 위로 스크롤
+  };
+
+  return (
+    <div className={styles.buttonContainer}>
+      <button className={styles.btn} onClick={handleClick}>
+        {text}
+      </button>
+    </div>
+  );
 }
 
 export default BottomButton;
