a12367bb69ea5330f26162a13dfb06e0d2a43869
hofkj <carrot070410@naver.com>
2025-06-09 10:45:35 +0900

    Feat : 예약 step1 api 연결

--- ohmykase/src/components/reservation/TimeSwiper.js
+++ ohmykase/src/components/reservation/TimeSwiper.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -6,44 +6,32 @@ import "swiper/css/pagination";
 import { FreeMode, Pagination } from "swiper/modules";
 
 import Time from '../reservation/Time';
-import styles from "../../styles/reservation/timeSwiper.module.css"; // CSS 모듈 임포트
+import styles from "../../styles/reservation/timeSwiper.module.css";
 
-function TimeSwiper() {
-    const [activeTime, setActiveTime] = useState(null);  // 클릭된 시간을 관리하는 상태
-
+function TimeSwiper({ activeTime, setActiveTime }) {
     const handleTimeClick = (time) => {
         setActiveTime(time);
     };
 
+    const times = ["11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
+
     return (
         <div className={styles.timeContainer}>
             <Swiper
-                slidesPerView={5.3} // 디자인과 물어보기!
+                slidesPerView={5.3}
                 freeMode={true}
                 modules={[FreeMode, Pagination]}
                 className={styles.swiper}
             >
-                <SwiperSlide>
-                    <Time time={"11:00"} isActive={activeTime === "11:00"} onClick={() => handleTimeClick("11:00")} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Time time={"12:00"} isActive={activeTime === "12:00"} onClick={() => handleTimeClick("12:00")} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Time time={"13:00"} isActive={activeTime === "13:00"} onClick={() => handleTimeClick("13:00")} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Time time={"14:00"} isActive={activeTime === "14:00"} onClick={() => handleTimeClick("14:00")} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Time time={"15:00"} isActive={activeTime === "15:00"} onClick={() => handleTimeClick("15:00")} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Time time={"16:00"} isActive={activeTime === "16:00"} onClick={() => handleTimeClick("16:00")} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Time time={"17:00"} isActive={activeTime === "17:00"} onClick={() => handleTimeClick("17:00")} />
-                </SwiperSlide>
+                {times.map((time) => (
+                    <SwiperSlide key={time}>
+                        <Time
+                            time={time}
+                            isActive={activeTime === time}
+                            onClick={() => handleTimeClick(time)}
+                        />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
