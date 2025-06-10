2b8a0381cfe9ed361712209524e96bf7d0e7b2b2
hofkj <carrot070410@naver.com>
2025-06-10 18:28:23 +0900

    Design : 예약이 없습니다. 문구, +코드 정리

--- ohmykase/src/pages/NavReservationPage.js
+++ ohmykase/src/pages/NavReservationPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import styles from "../styles/pages/NavReservationPage.module.css";
 import PageHeader from "../components/common/PageHeader";
 import Nav from "../components/common/Nav";
@@ -9,7 +8,7 @@ import axios from "axios";
 const apiKey = "7VCEB37-69B4CKZ-QV2674N-BTZTWXE";
 
 function NavReservationPage() {
-  const [activeTab, setActiveTab] = useState("upcoming"); 
+  const [activeTab, setActiveTab] = useState("upcoming");
   const [upcomingReservations, setUpcomingReservations] = useState([]);
   const [completedReservations, setCompletedReservations] = useState([]);
   const [cancelledReservations, setCancelledReservations] = useState([]);
@@ -22,6 +21,7 @@ function NavReservationPage() {
         axios.get(`http://localhost:3000/api/reservation/status/finished/${apiKey}`, { withCredentials: true }),
         axios.get(`http://localhost:3000/api/reservation/status/cancle/${apiKey}`, { withCredentials: true }),
       ]);
+
       setUpcomingReservations([...watingRes.data, ...okRes.data]);
       setCompletedReservations(finishedRes.data);
       setCancelledReservations(cancelledRes.data);
@@ -33,10 +33,6 @@ function NavReservationPage() {
   useEffect(() => {
     fetchReservations();
   }, []);
-
-  const handleTabClick = (tabName) => {
-    setActiveTab(tabName); 
-  };
 
   const renderReservations = (list) =>
     list.length > 0 ? (
@@ -50,9 +46,24 @@ function NavReservationPage() {
       <PageHeader text="예약" />
 
       <div className={styles.tabContainer}>
-        <div className={`${styles.tab} ${activeTab === "upcoming" ? styles.activeTab : ""}`} onClick={() => handleTabClick("upcoming")}>예정된 예약</div>
-        <div className={`${styles.tab} ${activeTab === "completed" ? styles.activeTab : ""}`} onClick={() => handleTabClick("completed")}>완료된 예약</div>
-        <div className={`${styles.tab} ${activeTab === "cancelled" ? styles.activeTab : ""}`} onClick={() => handleTabClick("cancelled")}>취소된 예약</div>
+        <div
+          className={`${styles.tab} ${activeTab === "upcoming" ? styles.activeTab : ""}`}
+          onClick={() => setActiveTab("upcoming")}
+        >
+          예정된 예약
+        </div>
+        <div
+          className={`${styles.tab} ${activeTab === "completed" ? styles.activeTab : ""}`}
+          onClick={() => setActiveTab("completed")}
+        >
+          완료된 예약
+        </div>
+        <div
+          className={`${styles.tab} ${activeTab === "cancelled" ? styles.activeTab : ""}`}
+          onClick={() => setActiveTab("cancelled")}
+        >
+          취소된 예약
+        </div>
       </div>
 
       <div className={styles.reservationList}>
