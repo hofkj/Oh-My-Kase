7679f7a87f7c5296b3223784663cdfb4566a2b2c
hofkj <carrot070410@naver.com>
2025-06-17 19:54:14 +0900

    Fix : 지도 페이지 오류 수정

--- ohmykase/src/pages/MapPage.js
+++ ohmykase/src/pages/MapPage.js
@@ -9,7 +9,8 @@ import styles from "../styles/pages/MapPage.module.css";
 
 export default function MapPage() {
   const location = useLocation();
-  const mapSrc = location.state?.mapSrc ||
+  const mapSrc =
+    location.state?.mapSrc ||
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7840459.679012818!2d131.39455198440953!3d42.61614138137222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34674e0fd77f192f%3A0xf54275d47c665244!2z7J2867O4!5e0!3m2!1sko!2skr!4v1747923963702!5m2!1sko!2skr";
   const shopId = location.state?.shopId || null;
 
@@ -30,16 +31,20 @@ export default function MapPage() {
         ></iframe>
       </div>
 
-      {/* <BottomSheet
+      <BottomSheet
         blocking={false}
-        snapPoints={({ maxHeight }) => [maxHeight * 0.9, maxHeight / 2, maxHeight * 0.2]}
+        snapPoints={({ maxHeight }) => [
+          maxHeight * 0.9,
+          maxHeight / 2,
+          maxHeight * 0.2,
+        ]}
         open={open}
         ref={sheetRef}
       >
         <div style={{ height: "100%", overflowY: "auto" }}>
           <MapItem shopId={shopId} />
         </div>
-      </BottomSheet> */}
+      </BottomSheet>
 
       <Nav
         home="/images/nav/home.png"
